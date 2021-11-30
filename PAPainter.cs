d08e55386df19850a14271a7cb26cba46035f590
Quistar <simon.ueng@outlook.com>
2021-11-30 20:52:45 +0800

    Ported prop painter

--- PAPainter.cs
+++ PAPainter.cs
@@ -1,7 +1,90 @@
 ﻿using ColossalFramework.UI;
+using EManagersLib;
+using MoveIt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using HarmonyLib;
 
 namespace PropAnarchy {
-    public class PAPainter : UIPanel {
+    internal static class PAPainter {
+        private const string PAINTERBTN_NAME = @"PAPainterButton";
+        private const string COLORFIELD_NAME = @"PAPainterColorField";
+        private const string COLORPICKER_NAME = @"PAPaintercolorPicker";
+        private const string MTP_NAME = @"mtpContainer";
 
+        internal static void AddPropPainterBtn(UIToolOptionPanel optionPanel, UIButton moreTools, UIPanel mtpBackGround, UIPanel mtpContainer) {
+            UIColorField field = Object.Instantiate(UITemplateManager.Get<UIPanel>("LineTemplate").Find<UIColorField>("LineColor"));
+            field.isVisible = true;
+            field.name = COLORFIELD_NAME;
+            UIColorPicker picker = Object.Instantiate(field.colorPicker);
+            picker.eventColorUpdated += (color) => {
+                HashSet<Instance> selections = Action.selection;
+                EPropInstance[] props = EPropManager.m_props.m_buffer;
+                if (!(selections is null) && selections.Count > 0) {
+                    foreach (var selection in selections) {
+                        uint propID = selection.id.GetProp32();
+                        if (selection.isValid && !selection.id.IsEmpty && propID > 0) {
+                            props[propID].m_color = color;
+                        }
+                    }
+                }
+            };
+            optionPanel.AttachUIComponent(picker.gameObject);
+            picker.color = Color.white;
+            picker.name = COLORPICKER_NAME;
+
+            UIPanel pickerPanel = picker.component as UIPanel;
+            pickerPanel.color = Color.white;
+            //pickerPanel.size = new Vector2(254f, 226f);
+            pickerPanel.backgroundSprite = @"InfoPanelBack";
+            pickerPanel.isVisible = false;
+            //pickerPanel.absolutePosition = optionPanel.m_viewOptions.absolutePosition - new Vector3(329, 147);
+            // re-adjust moretools panel
+            Vector2 containerSize = mtpContainer.size;
+            containerSize.y += 40f;
+            optionPanel.m_moreToolsPanel.size = containerSize;
+            mtpContainer.size = containerSize;
+            Vector2 backgroundSize = mtpBackGround.size;
+            backgroundSize.y += 40f;
+            mtpBackGround.size = backgroundSize;
+            optionPanel.m_moreToolsPanel.absolutePosition = moreTools.absolutePosition + new Vector3(0, 10 - optionPanel.m_moreToolsPanel.height);
+
+            UIMultiStateButton painterBtn = mtpContainer.AddUIComponent<UIMultiStateButton>();
+            painterBtn.name = PAINTERBTN_NAME;
+            painterBtn.cachedName = PAINTERBTN_NAME;
+            painterBtn.tooltip = PALocale.GetLocale(@"PainterTooltip");
+            painterBtn.playAudioEvents = true;
+            painterBtn.size = new Vector2(36f, 36f);
+            painterBtn.atlas = optionPanel.m_picker.atlas;
+            painterBtn.spritePadding = new RectOffset(2, 2, 2, 2);
+            painterBtn.backgroundSprites.AddState();
+            painterBtn.foregroundSprites.AddState();
+            painterBtn.backgroundSprites[0].normal = "OptionBase";
+            painterBtn.backgroundSprites[0].focused = "OptionBase";
+            painterBtn.backgroundSprites[0].hovered = "OptionBaseHovered";
+            painterBtn.backgroundSprites[0].pressed = "OptionBasePressed";
+            painterBtn.backgroundSprites[0].disabled = "OptionBaseDisabled";
+            painterBtn.foregroundSprites[0].normal = "EyeDropper";
+            painterBtn.backgroundSprites[1].normal = "OptionBaseFocused";
+            painterBtn.backgroundSprites[1].focused = "OptionBaseFocused";
+            painterBtn.backgroundSprites[1].hovered = "OptionBaseHovered";
+            painterBtn.backgroundSprites[1].pressed = "OptionBasePressed";
+            painterBtn.backgroundSprites[1].disabled = "OptionBaseDisabled";
+            painterBtn.foregroundSprites[1].normal = "EyeDropper";
+            painterBtn.activeStateIndex = 0;
+            Vector2 parentSize = painterBtn.parent.size;
+            painterBtn.parent.parent.size = new Vector2(parentSize.x, parentSize.y + 40f);
+            painterBtn.parent.size = painterBtn.parent.parent.size;
+            painterBtn.eventActiveStateIndexChanged += (_, index) => {
+                pickerPanel.isVisible = index == 1;
+            };
+            pickerPanel.absolutePosition = painterBtn.absolutePosition - new Vector3(pickerPanel.width, pickerPanel.height - 50f);
+        }
+
+
+        internal static void StartPostfix(UIToolOptionPanel __instance) {
+        }
     }
 }
---
+++ PAPatcher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using PropAnarchy.PLT;
+using MoveIt;
+using System.Reflection.Emit;
+using System.Reflection;
+using ColossalFramework.UI;
+
+namespace PropAnarchy {
+    internal static class PAPatcher {
+        private const string HARMONYID = @"com.quistar.PropAnarchy";
+
+        private static IEnumerable<CodeInstruction> UIToolOptionPanelStartTranspiler(IEnumerable<CodeInstruction> instructions) {
+            using (var codes = instructions.GetEnumerator()) {
+                while(codes.MoveNext()) {
+                    var cur = codes.Current;
+                    if(cur.opcode == OpCodes.Ldarg_0 && codes.MoveNext()) {
+                        var next = codes.Current;
+                        if(next.opcode == OpCodes.Ldstr && next.operand is string str && str.Equals("MoveIt_OthersBtn", StringComparison.Ordinal)) {
+                            yield return new CodeInstruction(OpCodes.Ldarg_0);
+                            yield return new CodeInstruction(OpCodes.Ldarg_0);
+                            yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(UIToolOptionPanel), "m_moreTools"));
+                            yield return new CodeInstruction(OpCodes.Ldloc_2);
+                            yield return new CodeInstruction(OpCodes.Ldloc_3);
+                            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PAPainter), nameof(PAPainter.AddPropPainterBtn)));
+                            yield return cur;
+                            yield return next;
+                        } else {
+                            yield return cur;
+                            yield return next;
+                        }
+                    } else {
+                        yield return cur;
+                    }
+                }
+            }
+        }
+
+        internal static void LateEnablePatches() {
+            Harmony harmony = new Harmony(HARMONYID);
+            harmony.Patch(AccessTools.Method(typeof(ToolController), @"SetTool"),
+                prefix: new HarmonyMethod(AccessTools.Method(typeof(ToolBar), nameof(ToolBar.SetToolPrefix))),
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(ToolBar), nameof(ToolBar.SetToolPostfix))));
+            harmony.Patch(AccessTools.Method(typeof(BeautificationPanel), @"OnButtonClicked"),
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(PropLineTool), nameof(PropLineTool.BeautificationPanelOnClickPostfix))));
+            harmony.Patch(AccessTools.Method(typeof(UIToolOptionPanel), nameof(UIToolOptionPanel.Start)), transpiler: new HarmonyMethod(AccessTools.Method(typeof(PAPatcher), nameof(UIToolOptionPanelStartTranspiler))));
+        }
+
+        internal static void LateDisablePatches() {
+            Harmony harmony = new Harmony(HARMONYID);
+            harmony.Unpatch(AccessTools.Method(typeof(ToolController), @"SetTool"), HarmonyPatchType.All, HARMONYID);
+            harmony.Unpatch(AccessTools.Method(typeof(BeautificationPanel), @"OnButtonClicked"), HarmonyPatchType.Postfix, HARMONYID);
+            harmony.Unpatch(AccessTools.Method(typeof(UIToolOptionPanel), nameof(UIToolOptionPanel.Start)), HarmonyPatchType.Transpiler, HARMONYID);
+        }
+    }
+}
