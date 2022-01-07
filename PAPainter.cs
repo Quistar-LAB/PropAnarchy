8fc4f885b8837ffba71b348bd519188fa482adab
Quistar <simon.ueng@outlook.com>
2022-01-07 12:16:57 +0800

    Re-organize delegate

--- PAPainter.cs
+++ PAPainter.cs
@@ -44,97 +44,100 @@ namespace PropAnarchy {
             SetPickerColorField(picker, GetColor(vector.x, vector.y, HSBField.width, HSBField.height, picker.hue));
         }
 
-        internal static void AddPropPainterBtn(UIToolOptionPanel optionPanel, UIButton moreTools, UIPanel mtpBackGround, UIPanel mtpContainer) {
-            SetPickerColorField = PAUtils.CreateSetter<UIColorPicker, Color>(typeof(UIColorPicker).GetField("m_Color", BindingFlags.Instance | BindingFlags.NonPublic));
+        internal static void Initialize() {
             EPropInstance[] props = EPropManager.m_props.m_buffer;
-            UIColorField field = UnityEngine.Object.Instantiate(UITemplateManager.Get<UIPanel>("LineTemplate").Find<UIColorField>("LineColor"));
+            SimulationManager smInstance = Singleton<SimulationManager>.instance;
+            SetPickerColorField = PAUtils.CreateSetter<UIColorPicker, Color>(typeof(UIColorPicker).GetField("m_Color", BindingFlags.Instance | BindingFlags.NonPublic));
+            UIColorField field = Object.Instantiate(UITemplateManager.Get<UIPanel>("LineTemplate").Find<UIColorField>("LineColor"));
             field.isVisible = true;
             field.name = COLORFIELD_NAME;
-            UIColorPicker picker = UnityEngine.Object.Instantiate(field.colorPicker);
-            optionPanel.AttachUIComponent(picker.gameObject);
+            UIColorPicker picker = Object.Instantiate(field.colorPicker);
             picker.color = Color.white;
             picker.name = COLORPICKER_NAME;
+            UIToolOptionPanel.AddMoreButtonCallback += (optionPanel, moreTools, mtpBackGround, mtpContainer) => {
+                if (Singleton<ToolManager>.instance.m_properties.m_mode != ItemClass.Availability.AssetEditor) {
+                    optionPanel.AttachUIComponent(picker.gameObject);
+                    UIPanel pickerPanel = picker.component as UIPanel;
+                    pickerPanel.color = Color.white;
+                    pickerPanel.backgroundSprite = @"InfoPanelBack";
+                    pickerPanel.isVisible = false;
+                    // re-adjust moretools panel
+                    Vector2 containerSize = mtpContainer.size;
+                    containerSize.y += 40f;
+                    optionPanel.m_moreToolsPanel.size = containerSize;
+                    mtpContainer.size = containerSize;
+                    Vector2 backgroundSize = mtpBackGround.size;
+                    backgroundSize.y += 40f;
+                    mtpBackGround.size = backgroundSize;
+                    optionPanel.m_moreToolsPanel.absolutePosition = moreTools.absolutePosition + new Vector3(0, 10 - optionPanel.m_moreToolsPanel.height);
 
-            UIPanel pickerPanel = picker.component as UIPanel;
-            pickerPanel.color = Color.white;
-            pickerPanel.backgroundSprite = @"InfoPanelBack";
-            pickerPanel.isVisible = false;
-            // re-adjust moretools panel
-            Vector2 containerSize = mtpContainer.size;
-            containerSize.y += 40f;
-            optionPanel.m_moreToolsPanel.size = containerSize;
-            mtpContainer.size = containerSize;
-            Vector2 backgroundSize = mtpBackGround.size;
-            backgroundSize.y += 40f;
-            mtpBackGround.size = backgroundSize;
-            optionPanel.m_moreToolsPanel.absolutePosition = moreTools.absolutePosition + new Vector3(0, 10 - optionPanel.m_moreToolsPanel.height);
+                    UIMultiStateButton painterBtn = mtpContainer.AddUIComponent<UIMultiStateButton>();
+                    painterBtn.name = PAINTERBTN_NAME;
+                    painterBtn.cachedName = PAINTERBTN_NAME;
+                    painterBtn.tooltip = PALocale.GetLocale(@"PainterTooltip");
+                    painterBtn.playAudioEvents = true;
+                    painterBtn.size = new Vector2(36f, 36f);
+                    painterBtn.atlas = optionPanel.m_picker.atlas;
+                    painterBtn.spritePadding = new RectOffset(2, 2, 2, 2);
+                    painterBtn.backgroundSprites.AddState();
+                    painterBtn.foregroundSprites.AddState();
+                    painterBtn.backgroundSprites[0].normal = "OptionBase";
+                    painterBtn.backgroundSprites[0].focused = "OptionBase";
+                    painterBtn.backgroundSprites[0].hovered = "OptionBaseHovered";
+                    painterBtn.backgroundSprites[0].pressed = "OptionBasePressed";
+                    painterBtn.backgroundSprites[0].disabled = "OptionBaseDisabled";
+                    painterBtn.foregroundSprites[0].normal = "EyeDropper";
+                    painterBtn.backgroundSprites[1].normal = "OptionBaseFocused";
+                    painterBtn.backgroundSprites[1].focused = "OptionBaseFocused";
+                    painterBtn.backgroundSprites[1].hovered = "OptionBaseHovered";
+                    painterBtn.backgroundSprites[1].pressed = "OptionBasePressed";
+                    painterBtn.backgroundSprites[1].disabled = "OptionBaseDisabled";
+                    painterBtn.foregroundSprites[1].normal = "EyeDropper";
+                    painterBtn.activeStateIndex = 0;
+                    Vector2 parentSize = painterBtn.parent.size;
+                    painterBtn.parent.parent.size = new Vector2(parentSize.x, parentSize.y + 40f);
+                    painterBtn.parent.size = painterBtn.parent.parent.size;
+                    painterBtn.eventActiveStateIndexChanged += (_, index) => {
+                        pickerPanel.isVisible = index == 1;
+                    };
+                    pickerPanel.absolutePosition = painterBtn.absolutePosition - new Vector3(pickerPanel.width, pickerPanel.height - 50f);
 
-            UIMultiStateButton painterBtn = mtpContainer.AddUIComponent<UIMultiStateButton>();
-            painterBtn.name = PAINTERBTN_NAME;
-            painterBtn.cachedName = PAINTERBTN_NAME;
-            painterBtn.tooltip = PALocale.GetLocale(@"PainterTooltip");
-            painterBtn.playAudioEvents = true;
-            painterBtn.size = new Vector2(36f, 36f);
-            painterBtn.atlas = optionPanel.m_picker.atlas;
-            painterBtn.spritePadding = new RectOffset(2, 2, 2, 2);
-            painterBtn.backgroundSprites.AddState();
-            painterBtn.foregroundSprites.AddState();
-            painterBtn.backgroundSprites[0].normal = "OptionBase";
-            painterBtn.backgroundSprites[0].focused = "OptionBase";
-            painterBtn.backgroundSprites[0].hovered = "OptionBaseHovered";
-            painterBtn.backgroundSprites[0].pressed = "OptionBasePressed";
-            painterBtn.backgroundSprites[0].disabled = "OptionBaseDisabled";
-            painterBtn.foregroundSprites[0].normal = "EyeDropper";
-            painterBtn.backgroundSprites[1].normal = "OptionBaseFocused";
-            painterBtn.backgroundSprites[1].focused = "OptionBaseFocused";
-            painterBtn.backgroundSprites[1].hovered = "OptionBaseHovered";
-            painterBtn.backgroundSprites[1].pressed = "OptionBasePressed";
-            painterBtn.backgroundSprites[1].disabled = "OptionBaseDisabled";
-            painterBtn.foregroundSprites[1].normal = "EyeDropper";
-            painterBtn.activeStateIndex = 0;
-            Vector2 parentSize = painterBtn.parent.size;
-            painterBtn.parent.parent.size = new Vector2(parentSize.x, parentSize.y + 40f);
-            painterBtn.parent.size = painterBtn.parent.parent.size;
-            painterBtn.eventActiveStateIndexChanged += (_, index) => {
-                pickerPanel.isVisible = index == 1;
-            };
-            pickerPanel.absolutePosition = painterBtn.absolutePosition - new Vector3(pickerPanel.width, pickerPanel.height - 50f);
-            SimulationManager smInstance = Singleton<SimulationManager>.instance;
-
-            /* Finally attach all delegates */
-            IEnumerator ProcessColor(HashSet<Instance> selections) {
-                yield return new WaitForSeconds(0.1f);
-                if (!(selections is null) && selections.Count > 0) {
-                    foreach (var selection in selections) {
-                        uint propID = selection.id.GetProp32();
-                        if (selection.isValid && !selection.id.IsEmpty && propID > 0) {
-                            props[propID].m_color = picker.color;
+                    /* Finally attach all delegates */
+                    IEnumerator ProcessColor(HashSet<Instance> selections) {
+                        yield return new WaitForSeconds(0.1f);
+                        if (!(selections is null) && selections.Count > 0) {
+                            foreach (var selection in selections) {
+                                uint propID = selection.id.GetProp32();
+                                if (selection.isValid && !selection.id.IsEmpty && propID > 0) {
+                                    props[propID].m_color = picker.color;
+                                }
+                            }
                         }
                     }
+
+                    picker.eventColorUpdated += (color) => smInstance.AddAction(ProcessColor(Action.selection));
+
+                    ActionAddHandler = (selections) => {
+                        smInstance.AddAction(() => {
+                            foreach (var selection in selections) {
+                                uint propID = selection.id.GetProp32();
+                                if (selection.isValid && !selection.id.IsEmpty && propID > 0) {
+                                    SetPickerColor(picker, props[propID].m_color);
+                                    break;
+                                }
+                            }
+                        });
+                    };
+
+                    ActionCloneHandler = (clonedOrigin) => {
+                        smInstance.AddAction(() => {
+                            foreach (KeyValuePair<Instance, Instance> x in clonedOrigin) {
+                                if (x.Key.id.Type != InstanceType.Prop) return;
+                                props[x.Value.id.GetProp32()].m_color = props[x.Key.id.GetProp32()].m_color;
+                            }
+                        });
+                    };
                 }
-            }
-
-            picker.eventColorUpdated += (color) => smInstance.AddAction(ProcessColor(Action.selection));
-
-            ActionAddHandler = (selections) => {
-                smInstance.AddAction(() => {
-                    foreach (var selection in selections) {
-                        uint propID = selection.id.GetProp32();
-                        if (selection.isValid && !selection.id.IsEmpty && propID > 0) {
-                            SetPickerColor(picker, props[propID].m_color);
-                            break;
-                        }
-                    }
-                });
-            };
-
-            ActionCloneHandler = (clonedOrigin) => {
-                smInstance.AddAction(() => {
-                    foreach (KeyValuePair<Instance, Instance> x in clonedOrigin) {
-                        if (x.Key.id.Type != InstanceType.Prop) return;
-                        props[x.Value.id.GetProp32()].m_color = props[x.Key.id.GetProp32()].m_color;
-                    }
-                });
             };
         }
     }
